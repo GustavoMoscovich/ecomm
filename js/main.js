41aabb4ed8bc2c3e904d95fb5bd4badf371a60d9
Gustavo M <gm.sis.it@gmail.com>
2023-01-10 16:26:55 -0300

    modifica nombre de jpg de fondo 02

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ const options = {
     //left: '32px', // default: 'unset'
     time: '0.5s', // default: '0.3s'
     mixColor: '#fff', // default: '#fff'
-    backgroundColor: '#fff',  // default: '#fff'
+    backgroundColor: 'linear-gradient(rgba(255, 255, 255, 0.95), rgba(255, 255, 255, 0.75)), url("../multimedia/pokemonfondo.jpg")',  // default: '#fff'
     buttonColorDark: '#100f2c',  // default: '#100f2c'
     buttonColorLight: '#fff', // default: '#fff'
     label: '🌓', // default: ''
